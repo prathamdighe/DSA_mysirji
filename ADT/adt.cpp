8690f62491cd947650e9f7399cc8de3bf541df5d
prathamdighe <54185895+prathamdighe@users.noreply.github.com>
2024-01-07 08:02:01 +0000

    delete function

--- ADT/adt.cpp
+++ ADT/adt.cpp
@@ -1,67 +1,103 @@
 #include <iostream>
 using namespace std;
 
-
 class Array
 {
-    private:
-        int capacity;
-        int lastIndex;
-        int *ptr;
-    public:
-        Array(int cap);
-        bool isEmpty();
-        bool isFull();
-        void append(int);
-        void insert(int,int);
-        void edit(int,int);
-};
+private:
+    int capacity;
+    int lastIndex;
+    int *ptr;
 
+public:
+    Array(int cap);
+    bool isEmpty();
+    bool isFull();
+    void append(int);
+    void insert(int, int);
+    void edit(int, int);
+    void del(int);
+};
 
 Array::Array(int cap)
 {
-    capacity=cap;
-    lastIndex=-1;
-    ptr=new int[cap];
+    capacity = cap;
+    lastIndex = -1;
+    ptr = new int[cap];
 }
 
-bool Array::isEmpty(){
-    return lastIndex==-1;
+bool Array::isEmpty()
+{
+    return lastIndex == -1;
 }
 
-bool Array::isFull(){
-    return lastIndex==capacity-1;
+bool Array::isFull()
+{
+    return lastIndex == capacity - 1;
 }
 
-void Array::append(int data){
-    if(isFull()){
-        cout<<"array is full";
+void Array::append(int data)
+{
+    if (isFull())
+    {
+        cout << "array is full";
     }
-    else{
+    else
+    {
         lastIndex++;
-        ptr[lastIndex]==data;
+        ptr[lastIndex] == data;
     }
 }
 
-void Array::insert(int index,int data){
-    int i=0;
-    if(isFull()){
-        cout<<endl<<"array is full";
+void Array::insert(int index, int data)
+{
+    int i = 0;
+    if (isFull())
+    {
+        cout << endl
+             << "array is full";
     }
-    else if(index<0 || index>lastIndex+1){
-        cout<<endl<<"cannot insert at the index";
+    else if (index < 0 || index > lastIndex + 1)
+    {
+        cout << endl
+             << "cannot insert at the index";
     }
-    else{
-        for(i=lastIndex;i>=index;i--){
-            ptr[i+1]=ptr[i];
+    else
+    {
+        for (i = lastIndex; i >= index; i--)
+        {
+            ptr[i + 1] = ptr[i];
         }
-        ptr[index]=data;
+        ptr[index] = data;
         lastIndex++;
     }
 }
 
-void Array::edit(int index,int data){
-    if(index>=0&&index<=lastIndex){
-        ptr[index]=data;
+void Array::edit(int index, int data)
+{
+    if (index >= 0 && index <= lastIndex)
+    {
+        ptr[index] = data;
+    }
+}
+
+void Array::del(int index)
+{
+    int i = 0;
+    if (isEmpty())
+    {
+        cout << endl
+             << "array is empty";
+    }
+    else if (index > lastIndex)
+    {
+        cout << endl
+             << "the index is empty";
+    }
+    else
+    {
+        for (i = index; i >= lastIndex; i++)
+        {
+            ptr[index] = ptr[index + 1];
+        }
     }
 }
