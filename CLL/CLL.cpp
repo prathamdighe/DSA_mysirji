5fbd060a1eba9a797cc6d9048c3397b2ef0549f0
shapedcharge2 <142771849+shapedcharge2@users.noreply.github.com>
2024-01-29 10:37:11 +0000

    CLL

---
+++ CLL/CLL.cpp
@@ -0,0 +1,141 @@
+#include <iostream>
+#include <stdio.h>
+using namespace std;
+
+struct node
+{
+    int item;
+    node *next;
+};
+
+class CLL
+{
+private:
+    node *last;
+
+public:
+    CLL();
+    void insertAtStart(int);
+    void insertAtLast(int);
+    node *search(int);
+    void insertAfter(node *, int);
+    void deleteFirst();
+    void deleteLast();
+};
+CLL::CLL()
+{
+    last = NULL;
+}
+
+void CLL::insertAtStart(int data)
+{
+    node *n = new node;
+    n->item = data;
+    if (last != NULL)
+    {
+        n->next = last->next;
+        last->next = n;
+    }
+    else
+    {
+        n->next = n;
+        last = n;
+    }
+}
+
+void CLL::insertAtLast(int data)
+{
+
+    node *n = new node;
+    n->item = data;
+    if (last != NULL)
+    {
+        n->next = last->next;
+        last->next = n;
+    }
+    else
+    {
+        n->next = n;
+    }
+    last = n;
+}
+
+node *CLL::search(int data)
+{
+    node *t;
+
+    if (last != NULL)
+    {
+        t = last;
+        do
+        {
+            t = t->next;
+            if (t->item == data)
+            {
+                return t;
+            }
+
+        } while (t != last);
+        return NULL;
+    }
+}
+
+void CLL::insertAfter(node *temp, int data)
+{
+
+    if (temp != NULL)
+    {
+
+        node *n = new node;
+        n->item = data;
+        n->next = temp->next;
+        temp->next = n;
+        if (temp == last)
+        {
+            last = n;
+        }
+    }
+}
+
+void CLL::deleteFirst()
+{
+    if (last != NULL)
+    {
+        node *temp = last->next;
+        if (last == last->next)
+        {
+            last = NULL;
+        }
+        else
+        {
+            last->next = temp->next;
+        }
+        delete temp;
+    }
+}
+
+void CLL::deleteLast()
+{
+
+    if (last != NULL)
+    {
+        node *t;
+        if (last == last->next)
+        {
+            t = last;
+            last = NULL;
+        }
+        else
+        {
+            node *temp = last;
+            while (temp->next != last)
+            {
+                temp = temp->next;
+            }
+            temp->next = last->next;
+            t = last;
+            last = temp;
+        }
+        delete t;
+    }
+}
