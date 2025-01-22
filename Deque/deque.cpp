fd96ff2e3bab1bda0d28297a641e2401d96c518e
prathamdighe <54185895+prathamdighe@users.noreply.github.com>
2025-01-22 08:33:58 +0000

    deque basics

--- Deque/deque.cpp
+++ Deque/deque.cpp
@@ -17,7 +17,14 @@ private:
 
 public:
     Deque();
-    void insert(int)
+    void insertfront(int);
+    void insertrear(int);
+    void deletefront();
+    void deleterear();
+    node *getfront();
+    node *getrear();
+    bool isempty();
+    ~Deque();
 };
 
 Deque::Deque()
@@ -25,7 +32,7 @@ Deque::Deque()
     rear = front = NULL;
 }
 
-void Deque::Deque(int data)
+void Deque::insertfront(int data)
 {
     node *n = new node;
     n->item = data;
@@ -41,3 +48,87 @@ void Deque::Deque(int data)
     }
     front = n;
 }
+
+void Deque::insertrear(int data)
+{
+    node *n = new node;
+    n->item = data;
+    n->prev = rear;
+    n->next = NULL;
+    if (front)
+    {
+        rear->prev = n;
+    }
+    else
+    {
+        front = n;
+    }
+    rear = n;
+}
+
+void Deque::deletefront()
+{
+    node *temp;
+    if (front != NULL)
+    {
+        temp = front;
+        if (front = rear)
+        {
+            front = rear = NULL;
+        }
+        else
+        {
+            front = front->next;
+            front->prev = NULL;
+        }
+        delete temp;
+    }
+}
+void Deque::deleterear()
+{
+    node *temp;
+    if (front != NULL)
+    {
+        temp = rear;
+        if (front = rear)
+        {
+            front = rear = NULL;
+        }
+        else
+        {
+            rear = rear->prev;
+            rear->next = NULL;
+        }
+        delete temp;
+    }
+}
+node *Deque::getfront()
+{
+    return front;
+}
+node *Deque::getrear()
+{
+    return rear;
+}
+bool Deque::isempty()
+{
+    if (front == NULL)
+    {
+        return true;
+    }
+    else
+    {
+        return false;
+    }
+}
+Deque::~Deque()
+{
+    while (front != NULL)
+    {
+        deletefront();
+    }
+}
+
+
+
+// create copy constructor and assignment copy consturctor.
