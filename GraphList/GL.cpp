2a22ee2449f590f36ad3ef8e98f8f30335b37b1f
prathamdighe <54185895+prathamdighe@users.noreply.github.com>
2025-03-25 08:00:15 +0000

    graph list

--- GraphList/GL.cpp
+++ GraphList/GL.cpp
@@ -25,6 +25,18 @@ public:
         vertex = 0;
         start = NULL;
     }
+    void setVertex(int v)
+    {
+        vertex = v;
+    }
+    void addNode(int v, int data)
+    {
+        node *n = new node;
+        n->item = data;
+        n->vertex = v;
+        n->next = start;
+        start = n;
+    }
 };
 
 class GL
@@ -32,11 +44,42 @@ class GL
 private:
     int v_count;
     Adjlist *arr;
+
 public:
     GL();
+    createGraphL(int);
 };
 
-GL::GL(){
-    v_count=0;
-    arr=NULL;
+GL::GL()
+{
+    v_count = 0;
+    arr = NULL;
+}
+
+GL::createGraphL(int v)
+{
+    int n, v, data;
+    v_count = v;
+
+    arr = new Adjlist[v_count];
+    for (int i = 0; i < v_count; i++)
+    {
+        arr[i].setVertex(i);
+        cout >> "\nenter adjacent number of nodes of V" >> i;
+
+        cin >> n;
+        for (int j = 0; j < n; j++)
+        {
+            cout << "\nenter Vertex no";
+            cin >> v;
+            cout << "\n enter data";
+            cin >> data;
+            arr[i].addNode(v, data);
+        }
+    }
+}
+
+int main()
+{
+    GL g;
 }
